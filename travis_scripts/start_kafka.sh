8af7a456b5abeb7c0334031a65109be4848a76e7
Ngo <ngomalalibo@yahoo.com>
2021-01-23 11:23:00 +0100

    Deployment to AWS EC2 with Travis CI

--- travis_scripts/start_kafka.sh
+++ travis_scripts/start_kafka.sh
@@ -1,6 +1,4 @@
-scp -i all-stock-key-pair.pem travis_scripts/start_kafka.sh ubuntu@ec2-3-124-206-1.eu-central-1.compute.amazonaws.com:.
-ssh -i "all-stock-key-pair.pem" ubuntu@ec2-3-120-98-15.eu-central-1.compute.amazonaws.com 'bash start_kafka.sh'
-cd kafka_2.12-2.5.0/
+cd kafka_2.12-2.5.0/ || exit
 kafka-server-stop.sh
 zookeeper-server-stop.sh
 nohup zookeeper-server-start.sh config/zookeeper.properties &
