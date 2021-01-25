61577cd99315d9346d0bc04458d7097def31345b
Ngo <ngomalalibo@yahoo.com>
2021-01-25 12:47:11 +0100

    Deployment to AWS EC2 with Travis CI

---
+++ travis_scripts/kafkainit.sh
@@ -0,0 +1,27 @@
+DAEMON_PATH=/home/ubuntu/kafka_2.12-2.5.0
+PATH=$PATH:$DAEMON_PATH/bin
+
+# See how we were called.
+# Stop daemons.
+echo "Shutting down Zookeeper"
+pid=$(ps ax | grep -i 'org.apache.zookeeper.server' | grep -v grep | awk '{print $1}')
+if [ -n "$pid" ]; then
+  kill -9 $pid
+else
+  echo "Zookeeper was not Running"
+fi
+echo "Shutting down Kafka"
+pid=$(ps ax | grep -i 'kafka.Kafka' | grep -v grep | awk '{print $1}')
+if [ -n "$pid" ]; then
+  kill -9 $pid
+else
+  echo "Kafka was not Running"
+fi
+# Start daemon.
+echo "Starting Zookeeper"
+nohup $DAEMON_PATH/bin/zookeeper-server-start.sh -daemon $DAEMON_PATH/config/zookeeper.properties 2>/dev/null &
+echo "Waiting for zookeeper to start"
+while ! nc -z localhost 2181; do sleep 0.1; done && \
+echo "Starting Kafka"
+nohup $DAEMON_PATH/bin/kafka-server-start.sh -daemon /$DAEMON_PATH/config/server.properties 2>/dev/null &
+exit 0
